609f3bbefc2de807ea2a829cbf791eda58b48a4a
Aliaksandr Bandarchyk <alex.band.agm@gmail.com>
2017-06-26 08:51:52 +0300

    CarDaoService become dependent on ContextClass

--- CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/CarDaoService.java
+++ CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/CarDaoService.java
@@ -2,9 +2,21 @@ package tdd.examples.cars;
 
 import java.util.List;
 
+
 /**
  * @author Aliaksandr Bandarchyk
  */
-public interface CarDaoService {
-	List<Car> getUnusedCars();
+public class CarDaoService {
+
+	private ContextClass contextClass;
+
+	public CarDaoService(ContextClass contextClass) {
+		this.contextClass = contextClass;
+	}
+
+	public List<Car> getUnusedCars() {
+		contextClass.doAction();
+		throw new RuntimeException(
+				"CarDaoService not suitable for test");
+	}
 }
--- CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/RouteService.java
+++ CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/RouteService.java
@@ -4,9 +4,6 @@ package tdd.examples.cars;
 /**
  * @author Aliaksandr Bandarchyk
  */
-public class RouteService {	
-	public int getDistance(int a, int b) {
-		throw new RuntimeException(
-				"RouteService not suitable for test");
-	}
+public interface RouteService {	
+	int getDistance(int a, int b);
 }
