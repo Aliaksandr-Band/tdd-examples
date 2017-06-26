8da8dbd82c7743ae25e84e5fcc24273e8c5e2411
Aliaksandr Bandarchyk <alex.band.agm@gmail.com>
2017-06-26 09:31:05 +0300

    minor changes in CarDaoService

--- CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/CarDaoService.java
+++ CarsUnitTestingExampleWithMockito/src/main/java/tdd/examples/cars/CarDaoService.java
@@ -7,16 +7,13 @@ import java.util.List;
  * @author Aliaksandr Bandarchyk
  */
 public class CarDaoService {
-
 	private ContextClass contextClass;
-
 	public CarDaoService(ContextClass contextClass) {
 		this.contextClass = contextClass;
 	}
 
 	public List<Car> getUnusedCars() {
 		contextClass.doAction();
-		throw new RuntimeException(
-				"CarDaoService not suitable for test");
+		return null;
 	}
 }
